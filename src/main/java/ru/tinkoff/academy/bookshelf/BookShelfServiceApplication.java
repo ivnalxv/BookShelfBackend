47bca231895930cb9804a490a0211780329a5aaa
Alexeev Ivan <boomiwi@gmail.com>
2022-11-02 10:59:13 +0300

    first commit

---
+++ src/main/java/ru/tinkoff/academy/bookshelf/BookShelfServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.academy.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShelfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShelfServiceApplication.class, args);
+	}
+
+}
